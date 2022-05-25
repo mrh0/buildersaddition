0841ae9a2982d3be10519790bf73a2e451151a6d
MRH0 <mrhmail1@gmail.com>
2022-05-26 00:55:10 +0200

    Update BuildersAddition.java

--- src/main/java/com/mrh0/buildersaddition/BuildersAddition.java
+++ src/main/java/com/mrh0/buildersaddition/BuildersAddition.java
@@ -20,7 +20,9 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
@@ -68,6 +70,7 @@ public class BuildersAddition {
     	
     	BOP_ACTIVE = ModList.get().isLoaded("biomesoplenty");
     	
+    	ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_CONFIG);
     	Config.loadConfig(Config.COMMON_CONFIG, FMLPaths.CONFIGDIR.get().resolve("buildersaddition-common.toml"));
 	}
 
